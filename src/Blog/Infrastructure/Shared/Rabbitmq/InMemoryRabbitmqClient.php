8e4eec78b301ce3f7395436abb26013d63d95b3e
zawiszaty <ZawiszaCzarny52@gmail.com>
2019-10-04 15:27:52 +0200

    fix test

---
+++ src/Blog/Infrastructure/Shared/Rabbitmq/InMemoryRabbitmqClient.php
@@ -0,0 +1,12 @@
+
+<?php
+declare(strict_types=1);
+
+
+namespace App\Blog\Infrastructure\Shared\Rabbitmq;
+
+
+final class InMemoryRabbitmqClient
+{
+
+}
---
+++ src/Blog/Infrastructure/Shared/Rabbitmq/RabbitmqClientInterface.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+
+namespace App\Blog\Infrastructure\Shared\Rabbitmq;
+
+
+interface RabbitmqClientInterface
+{
+
+}
